8f0818b2274946ea5432ac3bae35836a95c23835
Mohanad Hosam <nodyez14@gmail.com>
2022-07-08 10:13:19 +0300

    Edit build.sh to move the executable to bin/ so it can be ignored by git

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -xe
 CFLAGS="-Wall -Wextra -Werror -Wno-missing-braces -std=c17 -pedantic -ggdb `pkg-config --cflags sdl2 SDL2_ttf SDL2_mixer`"
 LIBS="`pkg-config --libs sdl2 SDL2_ttf SDL2_mixer` -lm"
 
-cc $CFLAGS -o pong main.c $LIBS
+cc $CFLAGS -o bin/pong main.c $LIBS
